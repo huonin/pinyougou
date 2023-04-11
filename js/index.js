1f12635d69372a1bd8c0e5342a5bfe29bb2634d2
huonin <honing1031@163.com>
2023-04-11 16:18:22 +0800

    4.11

--- js/index.js
+++ js/index.js
@@ -1,41 +1,18 @@
-// alert("Ahonen的个人网站正在搭建中。嘿嘿")
+alert("Ahonen的个人网站正在搭建中。嘿嘿")
 addEventListener("load", function () {
     const btnLift = document.querySelector('.arrow_l');
-    const btnRight = document.querySelector('.arrow_r')
-    for (let i = 1; i <4; i++) {
-        btnRight.addEventListener('click',function(){
-            var ul1 = document.querySelector('.ul1');
-            var ulLift = ul1.style.left;
-           
-                if (i==1&ul1.style.left==0) {
-                    ul1.style.left =- i * 100 +'%';
-                    return
-                }else if(i==2&ul1.style.left=='-100%'){
-                    ul1.style.left =- i * 100 +'%';
-                    return
-                }else if(i==3&ul1.style.left=='-200%'){
-                    ul1.style.left =- i * 100 +'%';
-                    return
-                }
-              
-                console.log(i);
-                // return
-        })
-        console.log(i);
-    }
-function picture() {
-    var ul = document.querySelector('.ul1 ')
-    ul.style.display = 'black';
-    var number = 100;
-    console.log(ul.style.left);
-    ul.style.left = '-200%';
-    console.log(1);
-    
-}
+    const btnRight = document.querySelector('.arrow_r');
+    const focus = document.querySelector('.focus');
 
-this.setInterval(function () {
-    // picture();
-}, 1000)
+    focus.addEventListener('mouseenter',function(){
+        btnLift.style.display = 'block';
+        btnRight.style.display = 'block';
+    })
+    focus.addEventListener('mouseleave',function(){
+        btnLift.style.display = 'none';
+        btnRight.style.display = 'none';
+    })
+    
 })
 
 
