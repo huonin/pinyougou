51006f82dbdedad470765b45336d580e9291878a
huonin <honing1031@163.com>
2023-04-12 19:02:49 +0800

    4.12

--- js/index.js
+++ js/index.js
@@ -8,10 +8,17 @@ addEventListener("load", function () {
     focus.addEventListener('mouseenter', function () {
         btnLift.style.display = 'block';
         btnRight.style.display = 'block';
+        clearInterval(timer);
+        timer = null;
     })
     focus.addEventListener('mouseleave', function () {
         btnLift.style.display = 'none';
         btnRight.style.display = 'none';
+        // 轮播效果
+        timer = setInterval(() => {
+            btn();
+            console.log(1);
+        }, 2500);
     })
     // 2.动态生成小圆圈，有几个图片就生成几个小圆圈。
     const ul = focus.querySelector('.ul1');
@@ -33,15 +40,36 @@ addEventListener("load", function () {
     // 轮播图片
     const li1 = document.querySelectorAll('.ul1 li');
     const li2 = document.querySelectorAll('.ul2 li');
+    // 克隆第一张图片
+    var first = li1[0].cloneNode(true);
+    console.log(first);
+    ul.appendChild(first);
+
+    // 点击小圆点更换图片
+    for (let index = 0; index < li2.length; index++) {
+        li2[index].addEventListener('click', function () {
+            animate(ul, index * -721)
+        })
+
+    }
     //前进
+    
+    var focusWidth = focus.style.width = 721;
+    console.log(focusWidth);
+    var num = 1;
     function btn() {
-        var bigLeft = ul.offsetLeft - 721;
-        console.log(ul.offsetLeft);
-        if (bigLeft >= (-721) * (li1.length - 1)) {
-            animate(ul, bigLeft);
-        } else if (bigLeft < (-721) * (li1.length - 1)) {
-            animate(ul, 0);
+        if (num == ul.children.length-2) {
+            ul.style.left = '-721px';
+            console.log(8);
+            num = 1;
         }
+        num++;
+        const bigLeft = -num * focusWidth;
+        animate(ul, bigLeft);
+        // else if (bigLeft < (-721) * (li1.length )) {
+        //     animate(ul, 0);
+        //     num = 0;
+        // }
         //圆点移动
         for (let i = 0; i < li2.length; i++) {
             let y = i + 1;
@@ -53,21 +81,27 @@ addEventListener("load", function () {
             }
         }
     }
-    btnRight.addEventListener('click',btn);
+    btnRight.addEventListener('click', btn);
 
-// 轮播效果
-    setInterval(() => {
+    // 轮播效果
+    timer = setInterval(() => {
         btn();
         console.log(1);
     }, 2500);
     //后退
+    // 克隆第五张图片
+    const fifth = li1[3].cloneNode(true);
+    console.log(fifth);
+    ul.prepend(fifth);
     btnLift.addEventListener('click', function () {
-        var smallLeft = ul.offsetLeft + 721;
-        if (smallLeft <= 0) {
-            animate(ul, smallLeft)
-        } else if (ul.offsetLeft == 0) {
-            animate(ul, (-721) * (li1.length - 1))
+        if (num == 0) {
+            ul.style.left = -3*focusWidth;
+            console.log(8);
+            num = 3;
         }
+        num++;
+        const smallLeft = num * focusWidth;
+        animate(ul, smallLeft);
         //圆点移动
         for (let i = li2.length; i > 0; i--) {
             let y = i - 1;
@@ -80,7 +114,12 @@ addEventListener("load", function () {
 
         }
     })
+    for (let index = 0; index < li2.length; index++) {
+        li2[index].addEventListener('click', function () {
 
+        })
+
+    }
 
 
 })
